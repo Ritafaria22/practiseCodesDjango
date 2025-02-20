c486d785f431c8a78a77c47a8f7a9d2c91a18009
Ritafaria22 <fariakabir507@gmail.com>
2025-02-20 11:35:55 +0600

    newcommit

--- musiciandirectory/musiciandirectory/views.py
+++ musiciandirectory/musiciandirectory/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from Album.models import Album
+
 def home(request):
-    data= Album.objects.all()
-    return render(request, 'home.html', {'data': data})
+     data= Album.objects.all()
+     return render(request, 'home.html', {'data': data})
